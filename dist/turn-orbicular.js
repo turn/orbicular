9d66a5693287f0d72ad31db240305fab7b50bfac
Ryan Chen <ryan.chen@turn.com>
2014-08-26 17:01:43 -0700

    Add dist file
    Signed-off-by: Ryan Chen <ryan.chen@turn.com>

---
+++ dist/turn-orbicular.js
@@ -0,0 +1,113 @@
+angular.module('turnOrbicularTemplate', ['turn-orbicular.html']);
+
+angular.module("turn-orbicular.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("turn-orbicular.html",
+    "<div class=\"co-circle-progress\"> \n" +
+    "    <div class=\"co-circle co-full\"> \n" +
+    "        <div class=\"co-fill\"></div> \n" +
+    "    </div> \n" +
+    "    <div class=\"co-circle co-half\"> \n" +
+    "        <div class=\"co-fill\"></div> \n" +
+    "        <div class=\"co-fill co-fix\"></div> \n" +
+    "    </div> \n" +
+    "    \n" +
+    "    <!-- Optional shadow -->\n" +
+    "    <div class=\"co-shadow\"></div> \n" +
+    "    <div class=\"co-content\"> \n" +
+    "        <div><div> \n" +
+    "            <div ng-transclude></div> \n" +
+    "        </div></div> \n" +
+    "    </div> \n" +
+    "</div>");
+}]);
+
+/**
+*    CoTag Orbicular
+*    A more or less pure CSS, circular, progress bar
+*    
+*   Copyright (c) 2014 CoTag Media.
+*    
+*    @author     Stephen von Takach <steve@cotag.me>
+*    @copyright  2014 cotag.me
+* 
+*     
+*     References:
+*        * http://fromanegg.com/post/41302147556/100-pure-css-radial-progress-bar
+*        * https://medium.com/@andsens/radial-progress-indicator-using-css-a917b80c43f9
+*
+**/
+/**
+ * Turn fork version of orbicular
+ * 1. Add multiple circule support
+ * 2. Add start rotation position configuration
+ * 3. Add hoverover tooltip
+ * 4. Add demo html
+ */
+angular.module('turn/turnOrbicular', ['turnOrbicularTemplate']).value('turnOrbicularOptions', {}).directive('turnOrbicular', [
+  '$window',
+  function (windowRef) {
+    // Constants here reduce memory requirements of each circle
+    var $window = angular.element(windowRef),
+      // Cache event strings
+      resizeEvents = 'orientationchange resize',
+      // Set the rotation of the square
+      updateProgress = function (circles, fix, pos) {
+        circles.css({
+          '-webkit-transform': 'rotate(' + pos + 'deg)',
+          '-moz-transform': 'rotate(' + pos + 'deg)',
+          '-ms-transform': 'rotate(' + pos + 'deg)',
+          '-o-transform': 'rotate(' + pos + 'deg)',
+          'transform': 'rotate(' + pos + 'deg)'
+        });
+        pos = pos * 2;
+        fix.css({
+          '-webkit-transform': 'rotate(' + pos + 'deg)',
+          '-moz-transform': 'rotate(' + pos + 'deg)',
+          '-ms-transform': 'rotate(' + pos + 'deg)',
+          '-o-transform': 'rotate(' + pos + 'deg)',
+          'transform': 'rotate(' + pos + 'deg)'
+        });
+      };
+    return {
+      templateUrl: 'turn-orbicular.html',
+      transclude: true,
+      replace: true,
+      restrict: 'EA',
+      scope: {
+        current: '=progression',
+        total: '='
+      },
+      link: function (scope, element, attrs) {
+        var circles = [], fix,
+          // Width must be an even number of pixels for the effect to work.
+          setWidth = function () {
+            var width = element.prop('offsetWidth');
+            element.css('font-size', width - width % 2 + 'px');
+          };
+        circles.push(element.children()[0]);
+        circles.push(angular.element(element.children()[0]).children()[0]);
+        circles.push(angular.element(element.children()[1]).children()[0]);
+        circles = angular.element(circles);
+        fix = angular.element(angular.element(element.children()[1]).children()[1]);
+        console.log('fix', fix);
+        // we have to use em's for the clip function to work like a percentage
+        // so we have to manually perform the resize based on width
+        setWidth();
+        scope.$on('orb width', setWidth);
+        // for programmatic resizing
+        // Optionally resize
+        if (attrs.hasOwnProperty('resize')) {
+          scope.$on('$destroy', function () {
+            $window.off(resizeEvents, setWidth);
+          });
+          $window.on(resizeEvents, setWidth);
+        }
+        // we watch for changes to the progress indicator of the parent scope
+        scope.$watch('current', function (newValue) {
+          newValue = newValue / scope.total * 180;
+          updateProgress(circles, fix, newValue);
+        });
+      }
+    };
+  }
+]);
